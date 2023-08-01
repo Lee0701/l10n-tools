d63f2be13523f36861db809d39ad74981365bfd4
Lee0701 <hgl1002@naver.com>
2023-08-02 07:22:08 +0900

    Fix stripping

--- src/functions.py
+++ src/functions.py
@@ -2,9 +2,10 @@
 def load_tbl(filename):
     with open(filename, 'r') as f:
         data = f.readlines()
-    data = [line[:-1] for line in data]
+    data = [line.replace('\r', '').replace('\n', '') for line in data]
     data = [line for line in data if len(line) > 0]
-    data = [line.split('=') for line in data]
+    seps = [line.index('=') for line in data]
+    data = [(line[:seps[i]], line[seps[i]+1:]) for i, line in enumerate(data)]
     data = [line for line in data if len(line) == 2]
     data = {int(k, 16): v for k, v in data}
     return data
