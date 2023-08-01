e93e3df78aa8de194f3f67ad47d308e0b0db6088
Lee0701 <hgl1002@naver.com>
2023-08-02 07:11:30 +0900

    Remove stripping when loading a tbl

--- src/functions.py
+++ src/functions.py
@@ -2,7 +2,6 @@
 def load_tbl(filename):
     with open(filename, 'r') as f:
         data = f.readlines()
-    data = [line.strip() for line in data]
     data = [line for line in data if len(line) > 0]
     data = [line.split('=') for line in data]
     data = [line for line in data if len(line) == 2]
