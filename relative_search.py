a25f0bc053a1ce3065a3f33fcea5b200d04a1b5b
Lee0701 <hgl1002@naver.com>
2023-08-01 22:11:38 +0900

    Fix offsets to be positive, reimplement text preview

--- relative_search.py
+++ relative_search.py
@@ -36,32 +36,43 @@ def main(input_file, search_str):
                 common_results[result_addr] = []
             common_results[result_addr].append((table_name, display_offset))
 
-    for result_addr, result_list in common_results.items():
+    common_results = sorted(common_results.items(), key=lambda x: len(x[1]), reverse=False)
+
+    for result_addr, result_list in common_results:
         if len(result_list) == 0:
             continue
 
+        raw_text = data[result_addr:result_addr+len(search_str)]
+        raw_text = ['$%02x' % c for c in raw_text]
+        raw_text = ' '.join(raw_text)
+
+        text = data[result_addr:result_addr+len(search_str)]
+
+        print('**', 'At', '$%04x' % result_addr)
         for result in result_list:
             table_name, display_offset = result
             pattern_table = pattern_tables[table_name]
-    
-            text = data[result_addr:result_addr+len(search_str)]
-            text = [c + display_offset for c in text]
-            text = [pattern_table[c] if c in range(len(pattern_table)) else '{%02x}' % c for c in text]
-            text = ''.join(text)
 
-            print('**', 'Table', table_name)
-            print('Addr', ':', '$%04x' % result_addr)
-            print('Offset', ':', display_offset)
-            print('Text', ':', text)
-            print()
+            sliced_data = data[result_addr:result_addr+len(search_str)]
+            sliced_data = [c - display_offset if isinstance(c, int) else c for c in sliced_data]
+            print(sliced_data)
+            text = [pattern_table[c] if isinstance(c, int) and c in range(len(pattern_table)) else text[i] for i, c in enumerate(sliced_data)]
+
+            print('Table', table_name, 'Offset:', '$%02x' % display_offset)
+
+        text = [c if isinstance(c, str) else '{%02x}' % c for c in text]
+        text = ''.join(text)
+        print('Text:', text)
+        print('Raw Text:', raw_text)
+        print()
 
 def search_pattern_table(search_str, pattern_table, data):
     # 檢索할 文字列의 인덱스들을 첫 文字에 對한 差異들의 패턴으로 表現한다.
     # None 값은 Don't care로 處理. 但, 有效한 글字가 두글字 未滿이면 檢索에서 除外한다.
     pattern = [pattern_table.index(c) if c in pattern_table else None for c in search_str]
     filtered = [d for d in pattern if d is not None]
-    offset = -filtered[0] if len(filtered) > 0 else None
-    pattern = [d + offset if d is not None else None for d in pattern]
+    offset = filtered[0] if len(filtered) > 0 else None
+    pattern = [d - offset if d is not None else None for d in pattern]
     if len([c for c in pattern if c is not None]) <= 1:
         pattern = []
 
@@ -77,9 +88,9 @@ def search_pattern(data, pattern):
     for index in range(len(data) - pattern_len):
         # 檢索할 패턴의 길이만큼 자른다.
         sliced_data = data[index : index+pattern_len]
-        offset = -sliced_data[first_not_none]
+        offset = sliced_data[first_not_none]
         # 잘라낸 部分을 그 첫 글字에 對한 오프셋들로 變換.
-        data_pattern = [d + offset for d in sliced_data]
+        data_pattern = [d - offset for d in sliced_data]
         data_pattern = [None if pattern[i] is None else data_pattern[i] for i in range(pattern_len)]
         # 두 값이 一致하면 檢索結果에 包含.
         if data_pattern == pattern:
@@ -92,5 +103,6 @@ if __name__ == '__main__':
     search_str = ' '.join(args)
     if len([c for c in search_str if c not in hiragana + hiragana_voiced + katakana + katakana_voiced + ' 　']) > 0:
         print('Only large hiragana/katakana/spaces are allowed in search.')
+        print('Fullwidth/halfwidth spaces are treated as wildacards.')
         exit()
     main(input_file, search_str)
